2fda97111ee90ae7ff6cc6f61efc91fa0891908c
manuel005 <mamacom.carlos@gmail.com>
2021-08-01 00:45:55 -0500

    cambios realizados

--- crud_django/settings.py
+++ crud_django/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'pw164687+=q@g%lnurer*_%hi1=!&j2l!5xck8*m2_g3wy$0!q'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['lista-estudiantes.azurewebsites.net']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -119,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
